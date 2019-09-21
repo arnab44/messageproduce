0f9a48f380e369f038c4b140c2f1d006ea503da8
Arnab <arnab.sen44@gmail.com>
2019-09-21 15:15:01 +0530

    update pom dependency

--- src/main/java/MessageProducerApplication.java
+++ src/main/java/MessageProducerApplication.java
@@ -4,7 +4,8 @@ public class MessageProducerApplication {
 
 
     public static  void main(String[] args) {
-        String  path  = "/Users/arnabs/Desktop/sampleData";
+       // String  path  = "/Users/arnabs/Desktop/sampleData";
+        String path = "/home/ec2-user/sampleData";
         Producer producer = new Producer(path);
         producer.readAndSendMessages();
     }
