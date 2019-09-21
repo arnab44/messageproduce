806f3b237d29c92e462e02f5b8460e993ba66a03
Arnab <arnab.sen44@gmail.com>
2019-09-22 00:47:56 +0530

    update source path

--- src/main/java/MessageProducerApplication.java
+++ src/main/java/MessageProducerApplication.java
@@ -5,7 +5,8 @@ public class MessageProducerApplication {
 
     public static  void main(String[] args) {
        // String  path  = "/Users/arnabs/Desktop/sampleData";
-        String path = "/home/ec2-user/sampleData";
+      //  String path = "/home/ec2-user/sampleData";
+        String path = "/data/finalDataSet4Sept2019/";
         Producer producer = new Producer(path);
         producer.readAndSendMessages();
     }
